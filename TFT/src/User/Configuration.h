3f0a67381c6afad09cbc1cddf8723da963f54903
Gerggg <42936100+Gerggg@users.noreply.github.com>
2020-03-06 11:33:04 +0100

    Update Configuration.h

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -44,11 +44,11 @@
 #define FAN_NUM      1    // set in 1~6
 
 //                       PLA      PETG       ABS
-#define PREHEAT_BED      {60,      70,       100}
-#define PREHEAT_HOTEND   {210,     250,      230}
+#define PREHEAT_BED      {60,      90,       100}
+#define PREHEAT_HOTEND   {205,     235,      230}
 #define PREHEAT_TEXT     {"PLA",  "PETG",   "ABS"}
 
-#define HEAT_MAX_TEMP    {120,    275,       275,       275,       275,       275,       275}    //max temperature can be set
+#define HEAT_MAX_TEMP    {110,    275,       275,       275,       275,       275,       275}    //max temperature can be set
 #define HEAT_SIGN_ID     {"B:",   "T0:",     "T1:",     "T2:",     "T3:",     "T4:",     "T5:"}
 #define HEAT_DISPLAY_ID  {"Bed",  "T0",      "T1",      "T2",      "T3",      "T4",      "T5"}
 #define HEAT_CMD         {"M140", "M104 T0", "M104 T1", "M104 T2", "M104 T3", "M104 T4", "M104 T5" };
